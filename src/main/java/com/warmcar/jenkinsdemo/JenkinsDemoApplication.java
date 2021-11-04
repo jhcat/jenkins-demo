d8dd571a1c44f35ee95c8e8371436521121806f8
jhcat <958013406@qq.com>
2021-11-04 13:26:40 +0800

    1:修改打包方式为war

--- src/main/java/com/warmcar/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/warmcar/jenkinsdemo/JenkinsDemoApplication.java
@@ -2,13 +2,15 @@ package com.warmcar.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class JenkinsDemoApplication {
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
@@ -19,4 +21,8 @@ public class JenkinsDemoApplication {
         return "hello jenkins";
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinsDemoApplication.class);
+    }
 }
